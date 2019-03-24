62e78952d3bb7fd5bd3d20cdc9111fb773ac7c6b
Chad Mayfield <chad@chd.my>
2019-03-24 16:33:36 -0600

    added copy of archives to list/archives dir, cleaned up variable names

--- create_blocklist_porn.pl
+++ create_blocklist_porn.pl
@@ -8,17 +8,23 @@
 use strict;
 use warnings;
 
-use LWP::Simple;
-use File::Fetch;
-use File::Slurp;
 use Archive::Tar;
 use Archive::Zip;
 use Array::Utils qw(:all);
+use File::Copy;
+use File::Fetch;
+use File::Slurp;
+use LWP::Simple;
+use POSIX qw(strftime);
 
+# get todays date
+#my $today = `date +%Y%m%d`;
+my $today = strftime "%Y%m%d", localtime;
+my $archive_dir = "lists/archives/upstream/$today";
 
 # we'll save the lists in these two files
-my $outfile = "lists/pi_blocklist_porn_top1m.list";
-my $allfile = "lists/pi_blocklist_porn_all.list";
+my $top1m_list = "lists/pi_blocklist_porn_top1m.list";
+my $all_list = "lists/pi_blocklist_porn_all.list";
 
 # whitelist valid domains that may make it though from upstream
 my $whitelistfile = "white.list";
@@ -44,9 +50,14 @@ foreach ($url1, $url2) {
     push @files, $filename;
 
     my $response = getstore($url, $filename);
-   die "ERROR: Couldn't download $filename!" unless defined $response;
+        die "ERROR: Couldn't download $filename!" unless defined $response;
 
     print "download complete: $filename\n";
+}
+
+# create dir for archive files
+unless ( -d "$archive_dir" ) {
+    mkdir "$archive_dir",0755 or die $!;
 }
 
 print "extracting files\n";
@@ -55,6 +66,9 @@ print "extracting files\n";
 foreach (@files) {
     my $filename = $_;
     my $suffix = ( split /\./, $filename )[-1];
+
+    # copy archive file to archive directory
+    copy("$filename","$archive_dir");
 
     print "extracting file: $filename\n";
 
@@ -77,19 +91,19 @@ foreach (@files) {
             # get rid of any IP's or non-domain words
             my $regexp = '^(xxx|porn|adult|sex|\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$';
 
-            open ALLFILE, ">", $allfile or die $!;
+            open LIST, ">", $all_list or die $!;
 
             # write all unmatched lines into tmpfile & load it into array
             for (@compare){
                 my $tmpline = $_;
                 next if ($_ =~ /$regexp/);
                 # write to file
-                print ALLFILE $tmpline;
+                print LIST $tmpline;
                 # load into array as well
                 push @valid, $_;
             }
 
-            close ALLFILE;
+            close LIST;
 
             print "  loaded successfully\n";
             
@@ -138,7 +152,7 @@ print "comparing lists for commonality\n";
 # get array intersections
 my @isect = intersect(@valid, @matches);
 
-open OUTFILE, ">", $outfile or die $!;
+open OUTFILE, ">", $top1m_list or die $!;
 print OUTFILE @isect;
 
 # TODO: fix this temp hack to add subdomains, it's fugly!
@@ -150,21 +164,21 @@ close OUTFILE;
 
 print "counting lines...";
 # get the line count for each line
-my $count1;
-open(FILE1, "< $allfile") or die "can't open $allfile: $!";
-$count1++ while <FILE1>;
-close FILE1;
+my $all_count;
+open(ALLFILE, "< $all_list") or die "can't open $all_list: $!";
+$all_count++ while <ALLFILE>;
+close ALLFILE;
 
 # get the line count for each line
-my $count2;
-open(FILE2, "< $outfile") or die "can't open $outfile: $!";
-$count2++ while <FILE2>;
-close FILE2;
+my $top1m_count;
+open(TOP1MFILE, "< $top1m_list") or die "can't open $top1m_list: $!";
+$top1m_count++ while <TOP1MFILE>;
+close TOP1MFILE;
 print "done\n";
 
 print "*******************************************************************\n";
-print "Light blocklist created: $outfile ($count2 lines)\n";
-print "Heavy blocklist created: $allfile ($count1 lines)\n";
+print "Light blocklist created: $top1m_list ($top1m_count lines)\n";
+print "Heavy blocklist created: $all_list ($all_count lines)\n";
 print "*******************************************************************\n";
 
 #EOF
